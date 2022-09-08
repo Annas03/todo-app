0a6c88f844cd8c668b83329cde8f59c2c5fcb2dd
Annas03 <baigannas9@gmail.com>
2022-09-08 18:43:40 +0500

    update taskList.js

--- src/components/taskList/taskList.js
+++ src/components/taskList/taskList.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react'
+import React,{useState} from 'react'
 import Task from "../task/Task.js"
 
 const TaskList = ({tasklist, SetTasklist}) => {
@@ -16,16 +16,15 @@ const TaskList = ({tasklist, SetTasklist}) => {
   function IsClearCompleteClicked(){
     SetTasklist(tasklist.filter((item) => !item.completed))
   }
-  useEffect(() => {console.log(tasklist)}, [tasklist])
 
   return (
     <div>
-      <div className='flex w-3/5 mx-auto text-xs py-1'>
+      <div className='flex w-44 max-w-xs sm:w-3/5 mx-auto text-xs py-1'>
         <input type='text' className='task w-full text-gray-400 bg-forms py-0.5 pl-1.5 rounded-l-md' placeholder='Create a new todo..' value={inputText} onChange={handleInput}></input>
         <button className='text-gray-500 bg-forms rounded-r-md pr-1 pb-0.5' onClick={UpdateTaskList}>save</button>
       </div>
 
-      <div className='mt-2 w-3/5 pt-0.5 mx-auto bg-forms text-gray-500'>  
+      <div className='mt-2 w-44 max-w-xs sm:w-3/5 pt-0.5 mx-auto bg-forms text-gray-500'>  
         {tasklist.length ? tasklist.map((t) => <Task key={t.value} topic={t.value} list={tasklist} updateTask = {SetTasklist}/>) : <h4 className='text-xs text-center'>Add task!</h4>}
         <div className='text-gray-500 text-xs flex justify-between mx-1'>
           <p><span>{tasklist.length}</span> items</p>
