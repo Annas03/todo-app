b2a69eab39975cbdce839cca66bf9aa4b2449270
Annas03 <baigannas9@gmail.com>
2022-09-09 19:03:52 +0500

    update Task.js

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -12,22 +12,13 @@ const Task = ({topic, list, updateTask}) => {
     updateTask(list.map((t) => t.value === topic ? {value: topic, completed:isCompleted}: t))}, [isCompleted])
 
   return (
-    <div className='flex w-11/12 mx-auto border-b border-gray-700 my-1 py-0.5'>
-        {isCompleted && 
+    <div className='flex w-11/12 mx-auto border-b border-gray-700 my-1 py-0.5'> 
         <div>
-          <button className='fixed flex self-center w-3 h-3 rounded-xl border border-purple-500 bg-blue-500' onClick={CompleteTask}>
-            <img className='w-2 h-2 pt-0.5 pl-0.5' src={check}></img>
+          <button className={`fixed flex self-center w-3 h-3 rounded-xl border border-purple-500 ${isCompleted ? 'bg-blue-500' : ''}`} onClick={CompleteTask}>
+            <img className={`${!isCompleted ? 'hidden' : ''} w-2 h-2 pt-0.5 pl-0.5`} src={check}></img>
           </button>
-          <p className='line-through break-all text-xs pl-4 text-gray-400'>{topic}</p>
+          <p className={`${isCompleted ? 'line-through' : ''} break-all text-xs pl-4 text-gray-400`}>{topic}</p>
         </div>
-        }
-        {!isCompleted && 
-        <div>
-          <button className='fixed flex self-center w-3 h-3 rounded-xl border border-purple-500' onClick={CompleteTask}>
-          <img className='hidden w-2 h-2 pt-0.5 pl-0.5' src={check}></img>
-        </button>
-        <p className='break-all text-xs pl-4 text-gray-400'>{topic}</p>
-        </div>}
     </div>
   )
 }
